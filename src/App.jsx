6e782917673bcdb7af4540c69bd7f926f1e5be99
nampphh28 <namphhe171518@fpt.edu.vn>
2025-04-06 23:35:44 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom"; // Thêm Outlet
 import Homepage from "./pages/Homepage/Homepage";
 import MainPage from "./mainPage";
 import Highschoolprogram from "./pages/Highschoolprogram/Highschoolprogram";
@@ -13,12 +13,12 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<MainPage />}>
-          <Route index element={<Homepage />} />
-          <Route path="/highschool" element={<Highschoolprogram />} />
-          <Route path="/highschool/OSSD" element={<OSSD />} />
-          <Route path="/highschool/Elite" element={<Eliteprogram />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/language-center" element={<LanguageCenter />} />
+          <Route index element={<Homepage />} /> {/* Trang chủ */}
+          <Route path="highschool" element={<Highschoolprogram />} />
+          <Route path="highschool/OSSD" element={<OSSD />} />
+          <Route path="highschool/Elite" element={<Eliteprogram />} />
+          <Route path="about" element={<About />} />
+          <Route path="language-center" element={<LanguageCenter />} />
         </Route>
       </Routes>
     </>
